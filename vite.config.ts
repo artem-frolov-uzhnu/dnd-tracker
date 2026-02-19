f415fdca0f5383f999fd395777d05508c34b857b
artem-frolov-uzhnu <artem.frolov@uzhnu.edu.ua>
2026-02-19 17:31:08 +0000

    Prep for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/dnd-tracker/',
   plugins: [react(), tailwindcss()],
 })
